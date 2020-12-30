21ee9b0eec7ab2e15f42009c548b64618e0caa85
Abraham George Smith <abe@abesmith.co.uk>
2020-12-30 18:31:11 +0100

    update create dataset to be mroe generic

--- datasets.py
+++ datasets.py
@@ -23,22 +23,25 @@ from data_prep import load_im_and_heart
 import random
 
 
-
-def create_train_val_split(data_dir):
+def create_train_val_test_split(data_dir):
     all_fnames = os.listdir(data_dir)
     # 34 in the train set and 8 in the validation set and 8 in the test set.
     random.seed(42)
     random.shuffle(sorted(all_fnames))
-    train_set = all_fnames[:34]
-    val_set = all_fnames[34:42]
-    test_set = all_fnames[42:]
-    assert len(train_set) == 34
-    assert len(val_set) == 8
-    assert len(test_set) == 8
+    total = len(all_fnames)
+    num_train = round(total * 0.6)
+    num_val = round(total * 0.2)
+    train_set = all_fnames[:num_train]
+    val_set = all_fnames[num_train:num_train+num_val]
+    test_set = all_fnames[num_train+num_val:]
+    assert len(train_set) == num_train
+    assert len(val_set) == num_val
+    assert len(test_set) == total - (num_train + num_val)
     return train_set, val_set, test_set
     
 
 class ImageDataset(Dataset):
+    """ Dataset of full images """
 
     def __init__(self, im_names, parent_folder_path):
         self.im_names = im_names
