61140f21a119492c1f8e81e7be5592a6441fdd0c
Abraham George Smith <abe@abesmith.co.uk>
2020-12-30 19:33:04 +0100

    add option to get random patch to enable training on high res images

--- datasets.py
+++ datasets.py
@@ -43,9 +43,13 @@ def create_train_val_test_split(data_dir):
 class ImageDataset(Dataset):
     """ Dataset of full images """
 
-    def __init__(self, im_names, parent_folder_path):
+    def __init__(self, im_names, parent_folder_path, patch_shape=None):
+        """ if patch_size is specified then take a random
+            patch with this size from the image and annotation """
+            
         self.im_names = im_names
         self.parent_dir = parent_folder_path
+        self.path_shape = patch_shape
 
     def __len__(self):
         return len(self.im_names)
@@ -53,15 +57,33 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
         im_name = self.im_names[idx]
         im, heart_labels = load_im_and_heart(os.path.join(self.parent_dir, im_name))
+        
+        if self.patch_shape:
+            # Limits for possible sampling locations from image
+            # (based on size of image)
+            depth_lim = im.shape[0] - patch_shape[0]
+            bottom_lim = im.shape[1] - patch_shape[1]
+            right_lim = im.shape[2] - patch_shape[2]
+
+            x_in = math.floor(random.random() * right_lim)
+            y_in = math.floor(random.random() * bottom_lim)
+            z_in = math.floor(random.random() * depth_lim)
+
+            im = image[z_in:z_in+patch_shape[0],
+                       y_in:y_in+patch_shape[1],
+                       x_in:x_in+patch_shape[2]]
+
         # add a channel dimension at the start, torch needs it
         im = np.expand_dims(im, axis=0).astype(np.float32)
         heart_labels = heart_labels.astype(np.int16)
         return im, heart_labels
 
 
-def create_datasets(data_dir):
-    train, val, test = create_train_val_split(data_dir)
-    train_dataset = ImageDataset(train, data_dir)
+def create_datasets(data_dir, patch_shape=None):
+    """ patch size is used for train images
+        to return a random patch from each image """
+    train, val, test = create_train_val_test_split(data_dir)
+    train_dataset = ImageDataset(train, data_dir, patch_shape)
     val_dataset = ImageDataset(val, data_dir)
     test_dataset = ImageDataset(test, data_dir)
     return train_dataset, val_dataset, test_dataset
