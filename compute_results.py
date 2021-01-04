b41d6746750e6c277a85e9b6348be4e5466cdf84
Abraham George Smith <abe@abesmith.co.uk>
2021-01-04 20:36:22 +0100

    computing baseline results

--- compute_results.py
+++ compute_results.py
@@ -66,6 +66,17 @@ def get_best_fine_cnn_for_run(run):
     cnn.eval()
     return cnn
 
+def get_best_cnn_for_run(exp_dir, run):
+    models_dir = os.path.join(exp_dir, 'runs', str(run), 'models')
+    best_model_fname = best_model_name(models_dir)
+    model_path = os.path.join(models_dir, best_model_fname) 
+    print('loading model from ', model_path)
+    cnn = torch.load(model_path).cuda()
+    #cnn = nn.DataParallel(cnn)
+    cnn.eval()
+    return cnn
+
+
 def get_cropped_heart(pad_loc_seg, pad_image, patch_shape):
     # pad before extracting cropped region to ensure sufficient context
     # for a heart near the boundary.
@@ -148,6 +159,36 @@ def two_stage_metrics(loc_cnn, fine_cnn, data_dir, im_names):
     return dataset_metrics, image_metrics_list
  
 
+def baseline_metrics(cnn, data_dir, im_names):
+    patch_shape = (64, 256, 256)
+    image_metrics_list = []
+    tps = []
+    tns = []
+    fps = []
+    fns = []
+
+    for im_name in im_names:
+        full_res_im, full_res_annot = load_im_and_heart(join(data_dir, im_name))
+        preds = segment(cnn, full_res_im.astype(np.float32),
+                        2, patch_shape, patch_shape)
+        foregrounds_int = full_res_annot.reshape(-1).astype(np.int)
+        preds_int = preds.reshape(-1).astype(np.int)
+        im_tps = (np.sum((foregrounds_int == 1) * (preds_int == 1)))
+        im_tns = (np.sum((foregrounds_int == 0) * (preds_int == 0)))
+        im_fps = (np.sum((foregrounds_int == 0) * (preds_int == 1)))
+        im_fns = (np.sum((foregrounds_int == 1) * (preds_int == 0)))
+        image_metrics = get_metrics(im_tps, im_fps, im_tns, im_fns)
+        print('metrics for', im_name, get_metrics_str(image_metrics))
+        image_metrics_list.append(image_metrics)
+        tps.append(im_tps)
+        tns.append(im_tns)
+        fps.append(im_fps)
+        fns.append(im_fns)
+
+    dataset_metrics = get_metrics(np.sum(tps), np.sum(fps), np.sum(tns), np.sum(fns))
+    return dataset_metrics, image_metrics_list
+
+
 def compute_two_stage_metrics():
     exp_dir = 'exp_output/results_collation_01'
     if not os.path.isdir(exp_dir):
@@ -168,5 +209,25 @@ def compute_two_stage_metrics():
         print(get_metric_csv_row(metrics, start) + f',{run},val', file=csv_file)
     print('csv saved to ', csv_path)
         
+
+
+def compute_baseline_metrics():
+    baseline_exp_dir = 'exp_output/struct_seg_heart_full'
+    exp_dir = 'exp_output/results_collation_01'
+    csv_path = os.path.join(exp_dir, 'baseline.csv')
+    csv_file = open(csv_path, 'w+')
+    print(get_metric_header_str() + ',run,dataset', file=csv_file)
+    data_dir = 'data/ThoracicOAR'
+    _, val_fnames, _ = create_train_val_test_split(data_dir)
+    for run in [0, 1, 2, 3, 4, 5]:
+        cnn = get_best_cnn_for_run(baseline_exp_dir, run)
+        start = time.time()
+        metrics, im_metrics_list = baseline_metrics(cnn, data_dir, val_fnames)
+        seg_dur = time.time() - start
+        print(get_metric_csv_row(metrics, start) + f',{run},val', file=csv_file)
+    print('csv saved to ', csv_path)
+
+
+
 if __name__ == '__main__':
-   compute_two_stage_metrics()
+   compute_baseline_metrics()
